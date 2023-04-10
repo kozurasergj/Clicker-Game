410085a58cff806335ec06c85935e65d9275a7ef
kozurasergj <kozura.sergj33@gmail.com>
2023-04-10 14:00:32 +0300

    added event Form submit

--- js/index.js
+++ js/index.js
@@ -5,12 +5,11 @@ import { checkInput } from './checkInput.js';
 window.addEventListener('DOMContentLoaded', () => {
   checkInput();
 
+
+
   const StartGameButton = document.querySelector('.start__game');
   StartGameButton.addEventListener('click', (event) => {
-    event.preventDefault(); 
-    if (form()) {
-      levels();;
-    }
+    form() && levels();
   });
 
   const returnHome = document.querySelector('.modal__page');
